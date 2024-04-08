d929a2bb0493dbfb3b938db035c6d7d0dea7ad8e
unknown <svaakwork@gmail.com>
2024-04-08 12:00:10 +0530

    footer responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -34,19 +34,19 @@ function Footer() {
     },
     {
       title: "Blogs",
-      links: [], // You can add links to blogs if available
+      links: [],
     },
     {
       title: "Contact Us",
-      links: [], // You can add contact information if available
+      links: [],
     },
   ];
 
   return (
-    <div className=" bg-black text-gray-400 flex flex-col gap-20 py-4 footer ">
+    <div className=" bg-black text-gray-400 flex flex-col gap-0 md:gap-20 py-4 footer ">
       <div className="flex justify-around flex-wrap footer1 pt-5">
         <div>
-          <img src="assets/images/navLogo.png" alt="" className="w-30" />
+          <img src="assets/images/navLogo.png" alt="" className="w-32" />
         </div>
         {sections.map((section, index) => (
           <div key={index} className="flex flex-col gap-3 uppercase">
@@ -57,41 +57,41 @@ function Footer() {
           </div>
         ))}
       </div>
-      <div className="flex flex-wrap justify-around footer2">
+      <div className="flex flex-wrap md:justify-around footer2">
         <span className="footer2_span">
-          <h3>CONNECT WITH US </h3>
+          <h3 className="text-[1.3rem]">CONNECT WITH US </h3>
           <span className="flex gap-3 text-2xl font-bold m-4 text-white">
-            <FaFacebookF  className="border p-1 rounded-full cursor-pointer"/>
-            <FaLinkedinIn className="border p-1 rounded-full cursor-pointer" />
-            <ImYoutube className="border p-1 rounded-full cursor-pointer" />
-            <BsTwitterX className="border p-1 rounded-full cursor-pointer" />
+            <FaFacebookF  className="border p-1 rounded-full cursor-pointer text-3xl"/>
+            <FaLinkedinIn className="border p-1 rounded-full cursor-pointer  text-3xl" />
+            <ImYoutube className="border p-1 rounded-full cursor-pointer text-3xl" />
+            <BsTwitterX className="border p-1 rounded-full cursor-pointer text-3xl" />
           </span>
         </span>
-        <div className="footer-bottom-text">
+        <div className="footer-bottom-text pl-[15px] border-b-2 md:border-b-0  md:border-l-2 border-[#FFDA14] ">
           <h4> Asia HQ </h4>
           <p>1108 Mahagun Maple</p>
           <p>F-26, Sector 50, Noida, UP- 201 301</p>
           <p>PH: +91 9818258199</p>
         </div>
-        <div className="footer-bottom-text">
+        <div className="footer-bottom-text pl-[15px] md:border-l-2 md:border-[#FFDA14]">
           <h4> USA HQ </h4>
           <p>702 Oberlin Road, Suite 200 Raleigh,</p>
           <p>NC 27605</p>
           <p>PH: +1 415 424 4644, 833-CRM-NEXT</p>
           <p>(Toll Free)</p>
         </div>
-        <div className="footer-bottom-text">
+        {/* <div className="footer-bottom-text pl-[15px] md:border-l-2 md:border-[#FFDA14]">
           <h4> Canada HQ </h4>
           <p>222 Bay St, Suite 3000; Toronto,</p>
           <p>Ontario, Canada, M5K 1E7</p>
         </div>
-        <div className="footer-bottom-text">
+        <div className="footer-bottom-text pl-[15px] md:border-l-2 md:border-[#FFDA14]">
           <h4> MEA HQ </h4>
           <p>Dubai Internet City G66,</p>
           <p>PH: +97 14 430 9318</p>
-        </div>
+        </div> */}
       </div>
-      <address className="flex justify-center text-xs">
+      <address className="flex justify-center text-xs pt-5 md:pt-0">
         © Copyright 2024 AIWIZE LABS PVT. LTD.
       </address>
     </div>
